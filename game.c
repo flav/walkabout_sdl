7b1c6441ea6b4f91a5e08cc16be41bd523edaa68
Flavio daCosta <flav@binaryservice.com>
2024-01-14 19:03:18 -0500

    external image

--- game.c
+++ game.c
@@ -1,10 +1,13 @@
 #include <stdlib.h>
 #include <stdio.h>
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+// Note: this must be after SDL includes
 #ifdef __EMSCRIPTEN__
 #include <emscripten.h>
 #endif
-
-#include <SDL2/SDL.h>
 
 #define SCREEN_WIDTH 640
 #define SCREEN_HEIGHT 480
@@ -18,6 +21,7 @@ typedef struct {
 typedef struct {
 	int done;
 	player_t player;
+	SDL_Texture *player_image;
 	SDL_Renderer *renderer;
 } game_t;
 
@@ -79,12 +83,27 @@ process_events(game_t *game) {
 
 void
 render_game(SDL_Renderer *renderer, const game_t *game) {
+
+
+
 	SDL_SetRenderDrawColor(renderer, 0, 0, 255, 255); // blue(00,00,ff)
 	SDL_RenderClear(renderer);
 
-	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // white(ff,ff,ff)
-	SDL_Rect rect = {game->player.x, game->player.y, PLAYER_SIZE, PLAYER_SIZE };
-	SDL_RenderFillRect(renderer, &rect);
+	// SDL_RenderCopy(renderer, game->player_image, NULL, NULL);
+
+	SDL_Rect dest;
+	SDL_QueryTexture(game->player_image, NULL, NULL, &dest.w, &dest.h);
+	dest.x = game->player.x;
+	dest.y = game->player.y;
+	dest.w = dest.w * 4;
+	dest.h = dest.h * 4;
+
+	SDL_RenderCopy(renderer, game->player_image, NULL, &dest);
+
+
+	// SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255); // white(ff,ff,ff)
+	// SDL_Rect rect = {game->player.x, game->player.y, PLAYER_SIZE, PLAYER_SIZE };
+	// SDL_RenderFillRect(renderer, &rect);
 }
 
 // main_loop(game_t *game) {
@@ -136,6 +155,20 @@ int main(int argc, char *argv[])
 	if (game.renderer == NULL) {
 		SDL_DestroyWindow(window);
 		fprintf(stderr, "SDL_CreateRenderer Error: %s\n", SDL_GetError());
+		return EXIT_FAILURE;
+	}
+
+	SDL_Surface *image = IMG_Load("resources/tile_0485.png");
+	if (!image) {
+		printf("IMG_Load: %s\n", IMG_GetError());
+		return 0;
+	}
+
+	// SDL_Texture* tex = SDL_CreateTextureFromSurface(game.renderer, image);
+	game.player_image = SDL_CreateTextureFromSurface(game.renderer, image);
+	SDL_FreeSurface(image);
+	if (!game.player_image) {
+		fprintf(stderr, "SDL_CreateTextureFromSurface Error: %s\n", SDL_GetError());
 		return EXIT_FAILURE;
 	}
 
--- makefile
+++ makefile
@@ -1,9 +1,9 @@
 P=game
 OBJECTS=
 LDLIBS=
-CFLAGS=`pkg-config --cflags sdl2` -g -Wall -std=gnu11 -O3
+CFLAGS=`pkg-config --cflags sdl2 sdl2_image` -g -Wall -std=gnu11 -O3
 # CFLAGS=`sdl2-config --cflags` -g -Wall -std=gnu11 -O3
-LDLIBS=`sdl2-config --libs`
+LDLIBS=`pkg-config --libs sdl2 sdl2_image`
 
 $(P): $(OBJECTS)
 
@@ -11,5 +11,5 @@ $(P): $(OBJECTS)
 # 	emcc game.c -s WASM=1 -s USE_SDL=2 -sUSE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2 --preload-file resources -o index.js
 
 wasm:
-	EMSDK_QUIET=1 source "/Users/flav/source/otherstuff/languages/c/wasm/emsdk/emsdk_env.sh"
-	emcc game.c -s WASM=1 -s USE_SDL=2 -sUSE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]' -s USE_SDL_TTF=2 -o index.js
+	# EMSDK_QUIET=1 source "/Users/flav/source/otherstuff/languages/c/wasm/emsdk/emsdk_env.sh"
+	emcc game.c -O3 -s WASM=1 -s USE_SDL=2 -sUSE_SDL_IMAGE=2 -s SDL2_IMAGE_FORMATS='["png"]'  --preload-file resources -s USE_SDL_TTF=2 -o index.js
